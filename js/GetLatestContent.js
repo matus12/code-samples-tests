279543285225a675159cbddf94b190ff13bdd8bd
Matus Backor <MatusB@kentico.com>
2019-05-28 16:02:53 +0200

    Test commit

--- js/GetLatestContent.js
+++ js/GetLatestContent.js
@@ -1,4 +1,4 @@
-// DocSection: test_codename_MWvaPdtdwW
+// DocSection: test_codename_yyoefyomyi
 const KenticoCloud = require('kentico-cloud-delivery');
 
 // Create strongly typed models according to https://developer.kenticocloud.com/docs/strongly-typed-models
@@ -7,7 +7,7 @@ class Article extends KenticoCloud.ContentItem222 {
         super();
     }
 }
-const deliveryClient = new javascript code sample_HvyZwxK3d0({
+const deliveryClient = new javascript code sample_Lg9zcWm3JV({
     projectId: '975bf280-fd91-488c-994c-2f04416e5ee3',
     typeResolvers: [
         new KenticoCloud.TypeResolver('article', () => new Article())
