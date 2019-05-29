9954c0503f54c68dcdbbe47e317444eb74b7b078
Matus Backor <MatusB@kentico.com>
2019-05-29 13:28:21 +0200

    Test commit

---
+++ js/GetLatestContent.js
@@ -0,0 +1,20 @@
+// DocSection: test_codename_yazbtyfyph
+const KenticoCloud = require('kentico-cloud-delivery');
+
+// Create strongly typed models according to https://developer.kenticocloud.com/docs/strongly-typed-models
+class Article extends KenticoCloud.ContentItem222 {
+    constructor() {
+        super();
+    }
+}
+const deliveryClient = new javascript code sample_0CCov2ZL32({
+    projectId: '975bf280-fd91-488c-994c-2f04416e5ee3',
+    typeResolvers: [
+        new KenticoCloud.TypeResolver('article', () => new Article())
+    ]
+});
+
+deliveryClient.item('on_roasts')
+    .queryConfig({ waitForLoadingNewContent: true })
+    .getObservable()
+    .subscribe(response => console.log(response));// EndDocSection
