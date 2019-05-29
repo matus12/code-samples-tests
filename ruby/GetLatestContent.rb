994a53c6c1f14da7c9b5e6eca209dc0e038ec48f
Matus Backor <MatusB@kentico.com>
2019-05-29 13:25:41 +0200

    Test commit

---
+++ ruby/GetLatestContent.rb
@@ -0,0 +1,10 @@
+# DocSection: test_codename_2bufpuel4z
+require 'delivery-sdk-ruby'
+
+delivery_client = KenticoCloud::Delivery::DeliveryClient.new project_id: '975bf280-fd91-488c-994c-2f04416e5ee3'
+delivery_client.item('on_roasts')
+               .request_latest_content
+               .execute do |response|
+                 item = response.item
+               endruby code sample_UqzFC8Vp2O
+# EndDocSection
